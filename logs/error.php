7f219438c8d7e515889f5a9b15512479c7264b53
emasia <masia.elena@gmail.com>
2015-03-24 17:37:31 +0100

    primo commit

---
+++ logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-03-24 09:28:28 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-03-24T09:28:28+00:00	INFO 127.0.0.1	joomlafailure	Password vuota non consentita
+2015-03-24T09:29:03+00:00	INFO 127.0.0.1	joomlafailure	Username e Password non coincidono o non hai ancora un account
+2015-03-24T09:29:28+00:00	INFO 127.0.0.1	joomlafailure	Username e Password non coincidono o non hai ancora un account
